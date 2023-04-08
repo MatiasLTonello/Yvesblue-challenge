24c0fff9ae63843ce08c694ece171b1009da75ad
matias <matias.tonello@gm2dev.com>
2023-04-08 02:11:26 -0300

    feat: sort and cursor pointer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,30 @@
-import DATA from "@/data.json";
+"use client";
 
-const tableRow = {
-  "Company Name": "Go-Ahead Group PLC",
-  "Total Revenue": "$4,571,628,564.26",
-  "Company Market Cap": "$655,937,334.00",
-  "Women Managers": 18,
-  "Women Employees": 14.4,
-  "ESG Score": 63.39998726,
-  "CO2 Scope 1 & 2 Adjusted": "946,540.00",
-  "CO2 Scope 1 & 2 Revenue Adjusted": 10.65,
-  "CO2 Scope 3 Adjusted": "48,700.00",
-  "CO2 Scope 3 Revenue Adjusted": 207.05,
-};
+import DATA from "@/data.json";
+import { useState, useMemo } from "react";
+
+export interface ImpactAssessmentData {
+  "Company Name": string;
+  "Total Revenue": string;
+  "Company Market Cap": string;
+  "Women Managers": number;
+  "Women Employees": number;
+  "ESG Score": number;
+  "CO2 Scope 1 & 2 Adjusted": number | string;
+  "CO2 Scope 1 & 2 Revenue Adjusted": number | string;
+  "CO2 Scope 3 Adjusted": number | string;
+  "CO2 Scope 3 Revenue Adjusted": number | string;
+}
 
 export default function Home() {
+  const [sort, setSort] = useState<keyof ImpactAssessmentData>("Company Name");
+  const matches = useMemo(() => {
+    return [...DATA].sort((a, b) => (a[sort] > b[sort] ? 1 : -1));
+  }, [sort]);
+
   return (
     <main>
-      <h1>Impact Assesment Data</h1>
+      <h1>Impact Assessment Data</h1>
       <table>
         <thead>
           <tr>
@@ -26,20 +34,28 @@ export default function Home() {
             <th colSpan={2}>CO2 SCOPE 3</th>
           </tr>
           <tr>
-            <th> Company Name </th>
-            <th>Total Compane Revenue</th>
-            <th>Market Capilazition</th>
-            <th>Managers</th>
-            <th>Employees</th>
-            <th>Total</th>
-            <th>Rev Adjusted</th>
-            <th>Total</th>
-            <th>Rev Adjusted</th>
-            <th>ESG Score</th>
+            <th onClick={() => setSort("Company Name")}> Company Name </th>
+            <th onClick={() => setSort("Total Revenue")}>
+              Total Compane Revenue
+            </th>
+            <th onClick={() => setSort("Company Market Cap")}>
+              Market Capilazition
+            </th>
+            <th onClick={() => setSort("Women Managers")}>Managers</th>
+            <th onClick={() => setSort("Women Employees")}>Employees</th>
+            <th onClick={() => setSort("CO2 Scope 1 & 2 Adjusted")}>Total</th>
+            <th onClick={() => setSort("CO2 Scope 1 & 2 Revenue Adjusted")}>
+              Rev Adjusted
+            </th>
+            <th onClick={() => setSort("CO2 Scope 3 Adjusted")}>Total</th>
+            <th onClick={() => setSort("CO2 Scope 3 Revenue Adjusted")}>
+              Rev Adjusted
+            </th>
+            <th onClick={() => setSort("ESG Score")}>ESG Score</th>
           </tr>
         </thead>
         <tbody>
-          {DATA.map((row, index) => (
+          {matches.map((row, index) => (
             <tr key={index}>
               <td>{row["Company Name"]}</td>
               <td>{row["Total Revenue"]}</td>
