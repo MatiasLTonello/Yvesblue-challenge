870bded05b5d851ce4af8d3bac9ea48529094f48
matias <matias.tonello@gm2dev.com>
2023-04-08 21:38:42 -0300

    feat: pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,145 +29,171 @@ const SANITIZED_DATA: ImpactAssessmentData[] = Array.from(
   FILTERED_DATA.values()
 );
 
+const PAGE_SIZE = 20;
+
 export default function Home() {
   const [sort, setSort] = useState<keyof ImpactAssessmentData>("Company Name");
+  const [page, setPage] = useState<number>(0);
   const matches = useMemo(() => {
     const numberRegex = new RegExp(/[\$(\)\,.]/g, "ig");
-    return [...SANITIZED_DATA].sort((a, b) => {
-      const aValue = Number(String(a[sort]).replace(numberRegex, ""));
-      const bValue = Number(String(b[sort]).replace(numberRegex, ""));
+    return [...SANITIZED_DATA]
+      .sort((a, b) => {
+        const aValue = Number(String(a[sort]).replace(numberRegex, ""));
+        const bValue = Number(String(b[sort]).replace(numberRegex, ""));
 
-      if (!Number.isNaN(aValue) && !Number.isNaN(bValue)) {
-        return bValue - aValue;
-      }
-      return (a[sort] as string).localeCompare(b[sort] as string);
-    });
-  }, [sort]);
+        if (!Number.isNaN(aValue) && !Number.isNaN(bValue)) {
+          return bValue - aValue;
+        }
+        return (a[sort] as string).localeCompare(b[sort] as string);
+      })
+      .slice(page * PAGE_SIZE, page * PAGE_SIZE + PAGE_SIZE);
+  }, [sort, page]);
 
   return (
     <main>
       <h1>Impact Assessment Data</h1>
-      <table>
-        <thead>
-          <tr>
-            <th colSpan={3} />
-            <th colSpan={2}>Women (Per 100)</th>
-            <th colSpan={2}>CO2 SCOPE 1 & 2</th>
-            <th colSpan={2}>CO2 SCOPE 3</th>
-          </tr>
-          <tr>
-            <th
-              className={sort === "Company Name" ? "font-bold" : "font-normal"}
-              onClick={() => setSort("Company Name")}
-            >
-              {" "}
-              Company Name{" "}
-            </th>
-            <th
-              className={sort === "Total Revenue" ? "font-bold" : "font-normal"}
-              onClick={() => setSort("Total Revenue")}
-            >
-              Total Compane Revenue
-            </th>
-            <th
-              className={
-                sort === "Company Market Cap" ? "font-bold" : "font-normal"
-              }
-              onClick={() => setSort("Company Market Cap")}
-            >
-              Market Capilazition
-            </th>
-            <th
-              className={
-                sort === "Women Managers" ? "font-bold" : "font-normal"
-              }
-              onClick={() => setSort("Women Managers")}
-            >
-              Managers
-            </th>
-            <th
-              className={
-                sort === "Women Employees" ? "font-bold" : "font-normal"
-              }
-              onClick={() => setSort("Women Employees")}
-            >
-              Employees
-            </th>
-            <th
-              className={
-                sort === "CO2 Scope 1 & 2 Adjusted"
-                  ? "font-bold"
-                  : "font-normal"
-              }
-              onClick={() => setSort("CO2 Scope 1 & 2 Adjusted")}
-            >
-              Total
-            </th>
-            <th
-              className={
-                sort === "CO2 Scope 1 & 2 Revenue Adjusted"
-                  ? "font-bold"
-                  : "font-normal"
-              }
-              onClick={() => setSort("CO2 Scope 1 & 2 Revenue Adjusted")}
-            >
-              Rev Adjusted
-            </th>
-            <th
-              className={
-                sort === "CO2 Scope 3 Adjusted" ? "font-bold" : "font-normal"
-              }
-              onClick={() => setSort("CO2 Scope 3 Adjusted")}
-            >
-              Total
-            </th>
-            <th
-              className={
-                sort === "CO2 Scope 3 Revenue Adjusted"
-                  ? "font-bold"
-                  : "font-normal"
-              }
-              onClick={() => setSort("CO2 Scope 3 Revenue Adjusted")}
-            >
-              Rev Adjusted
-            </th>
-            <th
-              className={sort === "ESG Score" ? "font-bold" : "font-normal"}
-              onClick={() => setSort("ESG Score")}
-            >
-              ESG Score
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {matches.map((row, index) => (
-            <tr key={index}>
-              <td>{row["Company Name"]}</td>
-              <td>{row["Total Revenue"]}</td>
-              <td>{row["Company Market Cap"]}</td>
-              <td>{row["Women Managers"]}</td>
-              <td>{row["Women Employees"]}</td>
-              <td>{row["CO2 Scope 1 & 2 Adjusted"]}</td>
-              <td>{row["CO2 Scope 1 & 2 Revenue Adjusted"]}</td>
-              <td>{row["CO2 Scope 3 Adjusted"]}</td>
-              <td>{row["CO2 Scope 3 Revenue Adjusted"]}</td>
-              <td className="flex gap-1 items-center justify-start">
-                <div style={{ width: 50 }}>
-                  {Math.round(row["ESG Score"] * 100) / 100}{" "}
-                </div>
-                <div
-                  style={{
-                    opacity: 0.2 + (1 - Math.round(row["ESG Score"]) / 100),
-                    backgroundColor: "#2f2fd8",
-                    height: 30,
-                    width: `${Math.round(row["ESG Score"])}%`,
-                  }}
-                />
-              </td>
+      <section className="overflow-x-auto">
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={3} />
+              <th colSpan={2}>Women (Per 100)</th>
+              <th colSpan={2}>CO2 SCOPE 1 & 2</th>
+              <th colSpan={2}>CO2 SCOPE 3</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            <tr>
+              <th
+                className={
+                  sort === "Company Name" ? "font-bold" : "font-normal"
+                }
+                onClick={() => setSort("Company Name")}
+              >
+                {" "}
+                Company Name{" "}
+              </th>
+              <th
+                className={
+                  sort === "Total Revenue" ? "font-bold" : "font-normal"
+                }
+                onClick={() => setSort("Total Revenue")}
+              >
+                Total Compane Revenue
+              </th>
+              <th
+                className={
+                  sort === "Company Market Cap" ? "font-bold" : "font-normal"
+                }
+                onClick={() => setSort("Company Market Cap")}
+              >
+                Market Capilazition
+              </th>
+              <th
+                className={
+                  sort === "Women Managers" ? "font-bold" : "font-normal"
+                }
+                onClick={() => setSort("Women Managers")}
+              >
+                Managers
+              </th>
+              <th
+                className={
+                  sort === "Women Employees" ? "font-bold" : "font-normal"
+                }
+                onClick={() => setSort("Women Employees")}
+              >
+                Employees
+              </th>
+              <th
+                className={
+                  sort === "CO2 Scope 1 & 2 Adjusted"
+                    ? "font-bold"
+                    : "font-normal"
+                }
+                onClick={() => setSort("CO2 Scope 1 & 2 Adjusted")}
+              >
+                Total
+              </th>
+              <th
+                className={
+                  sort === "CO2 Scope 1 & 2 Revenue Adjusted"
+                    ? "font-bold"
+                    : "font-normal"
+                }
+                onClick={() => setSort("CO2 Scope 1 & 2 Revenue Adjusted")}
+              >
+                Rev Adjusted
+              </th>
+              <th
+                className={
+                  sort === "CO2 Scope 3 Adjusted" ? "font-bold" : "font-normal"
+                }
+                onClick={() => setSort("CO2 Scope 3 Adjusted")}
+              >
+                Total
+              </th>
+              <th
+                className={
+                  sort === "CO2 Scope 3 Revenue Adjusted"
+                    ? "font-bold"
+                    : "font-normal"
+                }
+                onClick={() => setSort("CO2 Scope 3 Revenue Adjusted")}
+              >
+                Rev Adjusted
+              </th>
+              <th
+                className={sort === "ESG Score" ? "font-bold" : "font-normal"}
+                onClick={() => setSort("ESG Score")}
+              >
+                ESG Score
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {matches.map((row, index) => (
+              <tr key={index}>
+                <td>{row["Company Name"]}</td>
+                <td>{row["Total Revenue"]}</td>
+                <td>{row["Company Market Cap"]}</td>
+                <td>{row["Women Managers"]}</td>
+                <td>{row["Women Employees"]}</td>
+                <td>{row["CO2 Scope 1 & 2 Adjusted"]}</td>
+                <td>{row["CO2 Scope 1 & 2 Revenue Adjusted"]}</td>
+                <td>{row["CO2 Scope 3 Adjusted"]}</td>
+                <td>{row["CO2 Scope 3 Revenue Adjusted"]}</td>
+                <td className="flex gap-1 items-center justify-start">
+                  <div style={{ width: 50 }}>
+                    {Math.round(row["ESG Score"] * 100) / 100}{" "}
+                  </div>
+                  <div
+                    style={{
+                      opacity: 0.2 + (1 - Math.round(row["ESG Score"]) / 100),
+                      backgroundColor: "#2f2fd8",
+                      height: 30,
+                      width: `${Math.round(row["ESG Score"])}%`,
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+      <section>
+        <div className="flex gap-1 items-center justify-center">
+          {new Array(Math.ceil(SANITIZED_DATA.length / PAGE_SIZE))
+            .fill(true)
+            .map((_, index) => (
+              <button
+                className={page === index ? "font-bold" : "font-normal"}
+                key={index}
+                onClick={() => setPage(index)}
+              >
+                {index + 1}
+              </button>
+            ))}
+        </div>
+      </section>
     </main>
   );
 }
